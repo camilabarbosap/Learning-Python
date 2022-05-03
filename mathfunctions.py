dbd806bb8a82ff6b98831435e15d271cc8ffcf97
Camila Barbosa <91954589+camilabarbosap@users.noreply.github.com>
2022-05-03 14:47:58 +0000

    Math Functions

---
+++ mathfunctions.py
@@ -0,0 +1,22 @@
+import math
+
+#Fundamental Data Types
+#Integers and Float
+print(type(6)) 
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+
+#Math
+print(2 + 4) #soma
+print(2 - 4) #subtração
+print(2 * 4) #multiplicação
+print(2 / 4) #divisão
+print(2 ** 4) #potenciação
+print(5 // 2) #resultado inteiro da divisão
+print(5 % 4) #resto da divisão
+
+#Math functions
+print(round(3.9)) #arredondar 
+print(abs(-20)) #valor modular/absoluto
+ #more funcitons https://www.programiz.com/python-programming/modules/math
