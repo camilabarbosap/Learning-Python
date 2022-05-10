b98e4d507a8fd8c1252f3b2948b920d011fef8c5
Camila Barbosa <91954589+camilabarbosap@users.noreply.github.com>
2022-05-10 01:47:54 +0000

    strings

---
+++ Python Basics/augmentedassignmentoperator.py
@@ -0,0 +1,9 @@
+#Augmented Assignment Operator
+
+a = 5 # Precisa ser criada inicialmente
+a = 5 + 2
+a = a + 2
+a += 2 #O mesmo que somar 2 
+a -= 2 #O mesmo que diminuir 2 
+
+print(a)
---
+++ Python Basics/strings.py
@@ -0,0 +1,22 @@
+#Strings
+
+'hi hello there'
+"hi hello there"
+print(type('hello'))
+
+
+username = 'supercoder'
+password = 'supersecret'
+
+long_string = '''
+WOW
+O O
+___
+''' 
+
+print(long_string)
+
+first_name = 'Camila '
+last_name = 'Barbosa'
+full_name = first_name + last_name
+print(full_name)
